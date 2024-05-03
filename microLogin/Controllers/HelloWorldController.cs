3ea56eeffdd0ccaf4f2b380d6db15aa0d9087fa4
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2024-05-03 11:54:07 -0500

    Add project files.

---
+++ microLogin/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace microLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        // GET: api/<HelloWorldController>
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
